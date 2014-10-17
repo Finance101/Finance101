55a14830e51af8811db57180be171b77567e7cf8
Ryan Ingersoll <ryan@sridiversified.com>
2014-10-17 12:56:03 -0500

    cleanedup the layout somewhat, added a progress bar to transactions create page

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -20,44 +20,152 @@
 
 <body> 
 
-<!--  -->
-    @yield('content')
+@yield('top-script')
 
 
                
-        </div>
-
-            <!-- Blog Sidebar Widgets Column -->
-            <div class="col-xs-6 col-md-4">
-            {{ Form::open(array('action' => 'PostsController@index', 'class' => 'form-horizontal', 'method'=> 'GET')) }}
-<!-- {{$errors->has('title') ? 'has-error' : '' }} ternary-->
-               <label for="search">search: </label>
-               <input type="text" name="search" id="search"placeholder="search" value="{{{ Input::old('search') }}}" class= "form-control"> 
-               
-                <h2><input type="submit" value="search"></h2>
-                <!-- <textarea rows="3"></textarea> -->
-            {{ Form::close() }} 
-        </div>
-
-              
-                <div class="well">
-                    <h4>Finance 101 Categories</h4>
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#">transactions</a>
-                                </li>
-                                <li><a href="#">projections</a>
-                                </li>
-                                <li><a href="#">portfolios</a>
-                                </li>
-                            
-                            </ul>
-                        </div>
-                        
-                    </div>
-                    <!-- /.row -->
+     <div class="container">
+    <div class="row clearfix">
+        <div class="col-md-12 column">
+            <nav class="navbar navbar-default" role="navigation">
+                <div class="navbar-header">
+                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button> <a class="navbar-brand" href="#">Brand</a>
                 </div>
+                
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li class="active">
+                            <a href="#">Link</a>
+                        </li>
+                        <li>
+                            <a href="#">Link</a>
+                        </li>
+                        <li class="dropdown">
+                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown<strong class="caret"></strong></a>
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <a href="#">Action</a>
+                                </li>
+                                <li>
+                                    <a href="#">Another action</a>
+                                </li>
+                                <li>
+                                    <a href="#">Something else here</a>
+                                </li>
+                                <li class="divider">
+                                </li>
+                                <li>
+                                    <a href="#">Separated link</a>
+                                </li>
+                                <li class="divider">
+                                </li>
+                                <li>
+                                    <a href="#">One more separated link</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form class="navbar-form navbar-left" role="search">
+                        <div class="form-group">
+                            <input type="text" class="form-control" />
+                        </div> <button type="submit" class="btn btn-default">Submit</button>
+                    </form>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <a href="#">Link</a>
+                        </li>
+                        <li class="dropdown">
+                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown<strong class="caret"></strong></a>
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <a href="#">Action</a>
+                                </li>
+                                <li>
+                                    <a href="#">Another action</a>
+                                </li>
+                                <li>
+                                    <a href="#">Something else here</a>
+                                </li>
+                                <li class="divider">
+                                </li>
+                                <li>
+                                    <a href="#">Separated link</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                
+            </nav>
+            
+
+@yield('content')
+           
+
+            @yield('bottom-script')
+            <ul class="pagination">
+                <li>
+                    <a href="#">Prev</a>
+                </li>
+                <li>
+                    <a href="#">1</a>
+                </li>
+                <li>
+                    <a href="#">2</a>
+                </li>
+                <li>
+                    <a href="#">3</a>
+                </li>
+                <li>
+                    <a href="#">4</a>
+                </li>
+                <li>
+                    <a href="#">5</a>
+                </li>
+                <li>
+                    <a href="#">Next</a>
+                </li>
+            </ul> 
+        </div>
+    </div>
+
+    
+    <div class="row clearfix">
+        <div class="col-md-4 column">
+            <h2>
+                Budgets
+            </h2>
+            <p>
+                Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+            </p>
+            <p>
+                <a class="btn" href="#">View details »</a>
+            </p>
+        </div>
+        <div class="col-md-4 column">
+            <h2>
+                Projections
+            </h2>
+            <p>
+                Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+            </p>
+            <p>
+                <a class="btn" href="#">View details »</a>
+            </p>
+        </div>
+        <div class="col-md-4 column">
+            <h2>
+                Credit 
+            </h2>
+            <p>
+                Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+            </p>
+            <p>
+                <a class="btn" href="#">View details »</a>
+            </p>
+        </div>
+    </div>
+</div>
 
                 <!-- Side Widget Well -->
                 
@@ -78,7 +186,7 @@
             </div>
             <!-- /.row -->
         </footer>
-
+@stop
     </div>
     <!-- /.container -->
 
--- app/views/transactions/create.blade.php
+++ app/views/transactions/create.blade.php
@@ -1,1 +1,58 @@
-/vagrant/sites/finance101.dev/app/views/transactions/create.blade.php
+@extends('layouts.master') 
+
+
+ 
+                <!-- Post Content -->
+                  @section('top-script') 
+                    <h1>Transactions Page</h1>
+                  @stop
+
+                  @section('content')
+
+                   <div class="jumbotron">
+
+                        <div class="progress progress-striped active">
+                			<div class="progress-bar progress-success">
+                			</div>
+            			</div>
+                        
+                        {{ Form::open(array('action' => 'TransactionsController@store', 'class' => 'form-horizontal')) }}
+
+	                        {{ Form::text('title', null, array('placeholder' => 'Enter title...')) }}
+	                        
+	                        {{ Form::select('frequency', array(
+	                        	'daily' => 'Daily',
+	                        	'weekly' => 'Weekly',
+	                        	'monthly' => 'Monthly'
+	                        ), array('placeholder' => 'Choose frequency...')) }}
+	                        
+	                        {{ Form::number('amount', null, array('step' => 'any', 'min' => '0', 'placeholder' => 'Enter amount')) }}
+	                        
+	                        {{ Form::select('type', array(
+	                        	'debit' => 'Debit',
+	                        	'credit' => 'Credit'
+	                        ), array('placeholder' => 'Enter type...')) }}
+
+	                        {{ Form::submit() }}
+                        {{ Form::close() }}
+
+
+                            <!-- {{$errors->first('comment', '<span class="help-block">:message</span>')}} -->
+
+                        <p>{{{""}}}</p>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                        {{ Form::close() }} 
+ 					</div>
+ 				
+					@stop
+
+
+@section('bottom-script')
+<script type="text/javascript">
+    // 
+ </script>
+
+
+
+
+

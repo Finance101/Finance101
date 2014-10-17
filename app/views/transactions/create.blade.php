11b27e68b2cd374367cfecaeeb100a4dccd9212f
Frank Rodriguez <frank.aaron.rodriguez@gmail.com>
2014-10-17 15:38:46 -0500

    Fixing templating issue, simplified layout

--- app/views/transactions/create.blade.php
+++ app/views/transactions/create.blade.php
@@ -1,60 +1,33 @@
 @extends('layouts.master') 
 
+@section('content')
+	<h1>Transactions Page</h1>
 
- 
-                <!-- Post Content -->
-                  @section('top-script') 
-                    <h1>Transactions Page</h1>
-                  @stop
+	<div class="jumbotron">
+		<div class="progress">
+			<div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+			    60%
+			</div>
+		</div>
 
-                  @section('content')
+		{{ Form::open(array('action' => 'TransactionsController@store', 'class' => 'form-horizontal')) }}
+		    {{ Form::text('title', null, array('placeholder' => 'Enter title...')) }}
+		    
+		    {{ Form::select('frequency', array(
+		    	'' => 'Frequency',
+		    	'daily' => 'Daily',
+		    	'weekly' => 'Weekly',
+		    	'monthly' => 'Monthly'
+		    ), array('placeholder' => 'Choose frequency...')) }}
+		    
+		    {{ Form::number('amount', null, array('step' => 'any', 'min' => '0', 'placeholder' => 'Enter amount')) }}
+		    
+		    {{ Form::select('type', array(
+		    	'debit' => 'Debit',
+		    	'credit' => 'Credit'
+		    ), array('placeholder' => 'Enter type...')) }}
 
-                   <div class="jumbotron">
-
-                        <div class="progress">
-						  <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
-						    60%
-						  </div>
-						</div>
-                        
-                        {{ Form::open(array('action' => 'TransactionsController@store', 'class' => 'form-horizontal')) }}
-
-	                        {{ Form::text('title', null, array('placeholder' => 'Enter title...')) }}
-	                        
-	                        {{ Form::select('frequency', array(
-	                        	'' => 'Frequency',
-	                        	'daily' => 'Daily',
-	                        	'weekly' => 'Weekly',
-	                        	'monthly' => 'Monthly'
-	                        ), array('placeholder' => 'Choose frequency...')) }}
-	                        
-	                        {{ Form::number('amount', null, array('step' => 'any', 'min' => '0', 'placeholder' => 'Enter amount')) }}
-	                        
-	                        {{ Form::select('type', array(
-	                        	'debit' => 'Debit',
-	                        	'credit' => 'Credit'
-	                        ), array('placeholder' => 'Enter type...')) }}
-
-	                        {{ Form::submit() }}
-                        {{ Form::close() }}
-
-
-                            <!-- {{$errors->first('comment', '<span class="help-block">:message</span>')}} -->
-
-                        <p>{{{""}}}</p>
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                        {{ Form::close() }} 
- 					</div>
-
-					@stop
-
-
-@section('bottom-script')
-<script type="text/javascript">
-    // 
- </script>
-
-
-
-
-
+		    {{ Form::submit() }}
+		{{ Form::close() }}
+	</div>
+@stop

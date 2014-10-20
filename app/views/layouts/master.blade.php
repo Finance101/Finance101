2567f943b825eec271f3442b5038d28cb96ad7f2
klonekreyesm <mary.reyes00@gmail.com>
2014-10-20 15:41:24 -0500

    login layout created

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -1,9 +1,24 @@
-<html>
-<head>
-    <title>@yield('title')</title>
-</head>
-<body>
-	<h1>{{ Auth::id() }}</h1>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+    <title>{{ Auth::id() }}</title>
+
+    <!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+        
+    <!-- Custom styles for this template -->
+    <link href="/css/style.css" rel="stylesheet">
+    <link href="/css/style-responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
 
 	<ul>
 		@if(Auth::check())
@@ -21,6 +36,37 @@
 			<li><a href="{{ action('HomeController@showLogin') }}">Login</a></li>
 		@endif
 	</ul>
+
+
     @yield('content')
+
+
+
+
+<!-- js library-->
+<script src="js/jquery.js"></script>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+<!-- backstretch background library -->
+<script type="text/javascript" src="/js/jquery.backstretch.min.js"></script>
+
+<!-- script to stretch background image -->
+
+
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+    

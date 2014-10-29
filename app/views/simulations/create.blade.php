131a145f5360c513fc7fe95153d1bf273077a9b2
Frank Rodriguez <frank.aaron.rodriguez@gmail.com>
2014-10-29 14:22:41 -0500

    Updated filters to allow AJAX posts
    Updated simulation create form to utilize AJAX for quick transaction posting

--- app/views/simulations/create.blade.php
+++ app/views/simulations/create.blade.php
@@ -5,15 +5,10 @@
 			$sim_ids_array[$simulation->id] = $simulation->title;
 		} -->
 	<h1>Welcome to Finance 101</h1>
+	
 	<h3>Budget Your Future</h3>
-
-	<p>
-	<a id="newBudget" class="btn btn-primary popup" title="Budgets Are Important"  
-	  data-container="body" data-toggle="popover" data-placement="top" 
-	  data-content="They are the blueprint for financial success, please give your budget a name.">
-		CREATE A NEW BUDGET
-	</a>
-	</p>
+	
+	<p><a id="newBudget" class="btn btn-primary popup" title="Budgets Are Important" data-container="body" data-toggle="popover" data-placement="top" data-content="They are the blueprint for financial success, please give your budget a name.">CREATE A NEW BUDGET</a></p>
 
 <!-- Move to chart page -->
 	 <!-- <p><a id="newExpense" class="btn btn-primary popup" title="Create multiple expenses."  
@@ -26,65 +21,60 @@
 	  data-content="If you don't have an income be creative and come up with one.">CREATE AN INCOME</a><p>  -->
 
 	{{ Form::open(array('action' => 'SimulationsController@store', 'class' => 'form-horizontal')) }}
-	<div id='title1' class="form-group">
-		<div class="col-md-4">
-			{{ Form::text('title', null, array('placeholder' => 'Title for Budget 1', 'class' => 'form-control')) }}
+		<div id='title1' class="form-group form-piece">
+			<div class="col-md-4">
+				{{ Form::text('title', null, array('placeholder' => 'Title for Budget 1', 'class' => 'form-control input_field', 'id' => 'simulation_title')) }}
+			</div>
+			<div class="col-md-2">
+				<a id="step1btn" class="btn btn-primary popup" title="Congratulations You Now Have a Name for Your First Budget" data-container="body" data-toggle="popover" data-placement="right" data-content="We are going to create a series of transactions to simulate how a budget is created. First let's start with expenses. Please tell us what kind of expense you are creating, i.e, rent, car pmt, electric bill, etc.">STEP 1</a>
+			</div>
 		</div>
-		<div class="col-md-2">
-			<a id="step1btn" class="btn btn-primary popup" title="Congratulations You Now Have a Name for Your First Budget"  
-			  data-container="body" data-toggle="popover" data-placement="right" 
-			  data-content="We are going to create a series of transactions to simulate how a budget is created. First let's start with expenses. Please tell us what kind of expense you are creating, i.e, rent, car pmt, electric bill, etc.">
-				STEP 1
-			</a>
+
+		<div id='expenseName1' class="form-group form-piece transaction_form">
+			<div class="col-md-4">
+				{{ Form::select('title', array('Rent' => 'Rent', 'Car Payment' => 'Car Payment', 'Electric Bill' => 'Electric Bill', 'Student Loans' => 'Student Loans', 'Insurance' => 'Insurance'), null, array('class' => 'form-control input_field', 'id' => 'transaction_title')) }}
+			</div>
+			
+			<div class="col-md-2">
+				<a id="step2btn" class="btn btn-primary popup" title="Great Job!" data-container="body" data-toggle="popover" data-placement="right" data-content="Now that we have an expense type, please tell us how much $$ the expense is per occurance. i.e., $349.56 ">STEP 2</a>
+			</div>
 		</div>
-	</div>
 
-	<div id='expenseName1' class="form-group">
-		<div class="col-md-4">
-			{{ Form::select('type', array('rent', 'car payment', 'electric bill', 'student loans', 'insurance'), null, array('class' => 'form-control')) }}
+
+		<div id='expenseAmount1'class="form-group form-piece transaction_form">
+			<div class="col-md-4">
+				{{ Form::text('amount', null, array('placeholder' => 'Expense Amount','class' => 'form-control input_field', 'id' => 'transaction_amount')) }}
+			</div>
+			
+			<div class="col-md-2">
+				<a id="step3btn" class="btn btn-primary popup" title="Next we need to know how often your expense will occur." data-container="body" data-toggle="popover" data-placement="right" data-content="For example, annual, quarterly, monthly, bi-weekly, weekly or daily.">STEP 3</a>
+			</div>
 		</div>
-		<div class="col-md-2">
-			<a id="step2btn" class="btn btn-primary popup" title="Great Job!"  
-			  data-container="body" data-toggle="popover" data-placement="right" 
-			  data-content="Now that we have an expense type, please tell us how much $$ the expense is per occurance. i.e., $349.56 ">
-				STEP 2
-			</a>
+		
+		<div id="expense_type" class="form-group form-piece transaction_form">
+			<div class="col-md-4">
+				{{ Form::select('type', array('credit' => 'Credit', 'debit' => 'Debit'), null ,array('class' => 'form-control input_field', 'id' => 'transaction_type')) }}
+			</div>
+			
+			<div class="col-md-2">
+				<a id="step4btn" class="btn btn-primary popup" title="Gi">STEP $</a>
+			</div>
 		</div>
-	</div>
-
-	<div id='expenseAmount1'class="form-group">
-		<div class="col-md-4">
-			{{ Form::text('amount', null, array('placeholder' => 'Expense Amount','class' => 'form-control')) }}
+		
+		<div id='expenseFrequency1' class="form-group form-piece transaction_form">
+			<div class="col-md-4">
+				{{ Form::select('frequency', array('daily' => 'Daily', 'weekly' => 'Weekly', 'monthly' => 'Monthly'), null, array('class' => 'form-control input_field', 'id' => 'transaction_frequency')) }}
+			</div>
+			
+			<div class="col-md-2">
+				<button id="submitExpensebtn" class="btn btn-primary popup" title="You are all finished creating the first expense for your simulated budget."  
+				  data-container="body" data-toggle="popover" data-placement="right" 
+				  data-content="You can either create another expense item or an income item " type="submit">
+					Submit Expense
+				</button>
+			</div>
 		</div>
-		<div class="col-md-2">
-			<a id="step3btn" class="btn btn-primary popup" title="Next we need to know how often your expense will occur."  
-			  data-container="body" data-toggle="popover" data-placement="right" 
-			  data-content="For example, annual, quarterly, monthly, bi-weekly, weekly or daily.">
-			  	STEP 3
-			</a>
-		</div>
-	</div>
-	<div id='expenseFrequency1' class="form-group">
-		<div class="col-md-4">
-			{{ Form::select('frequency', array('Daily', 'Weekly', 'Bi-Weekly', 'Monthly'), null, array('class' => 'form-control')) }}
-		</div>
-		<div class="col-md-2">
-			<button id="submitExpensebtn" class="btn btn-primary popup" title="You are all finished creating the first expense for your simulated budget."  
-			  data-container="body" data-toggle="popover" data-placement="right" 
-			  data-content="You can either create another expense item or an income item " type="submit">
-				Submit Expense
-			</button>
-		</div>
-	</div>
-
-{{ Form::close() }}
-
-
-
-
-
-
-
+	{{ Form::close() }}
 <!-- Move to chart page -->
 <!-- <div id='incomeAmount1'>
 
@@ -145,106 +135,89 @@
 
 </div> --> 
 @stop
+
 @section('bottom-script')
-	  <script>
+  	<script>
+		$( document ).ready(function() {
+			var simulation_id;
+			$('.form-piece').hide();
+			
+			$('[data-toggle="popover"]').popover();
 
-				  $( document ).ready(function() {
-						$("#title1").hide();
-						$("#transaction1").hide();
-						$('#expenseName1').hide();
-						$('#expenseAmount1').hide();
-						$('#expenseFrequency1').hide();
-						$('#incomeAmount1').hide();
-						$('#incomeFrequency1').hide();
-						$('#newExpense').hide();
-						$('#newExpenseAmount').hide();
-						$('#newExpenseName').hide();
-						$('#newExpenseFrequency').hide();
-						$('#newExpense').hide();
-						$('#addIncome').hide();
-
-					$('[data-toggle="popover"]').popover();
-
-					$('body').on('click', function (e) {
-						$('[data-toggle="popover"]').each(function () {
-							//the 'is' for buttons that trigger popups
-							//the 'has' for icons within a button that triggers a popup
-							if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
-								$(this).popover('hide');
-							}
-						});
-					});
-					
-					function title1() {
-						$("#title1").slideDown(1000);
-
-						};
-
-					function expenseName1() {
-						$("#expenseName1").slideDown(1000);
-						};
-
-					function newExpense() {
-						
-						$("#newExpenseName").slideDown(1000);
-						$("#newExpenseAmount").slideDown(1000);
-						$("#newExpenseFrequency").slideDown(1000);
-
-						};
-					function submitNewExpense(){
-
+			$('body').on('click', function (e) {
+				$('[data-toggle="popover"]').each(function () {
+					//the 'is' for buttons that trigger popups
+					//the 'has' for icons within a button that triggers a popup
+					if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+						$(this).popover('hide');
 					}
-
-					$('#newBudget').click(function() {
-							title1();
-						}); 
-
-					$("#step1btn").click(function() {
-
-						expenseName1();
-					}); 
-
-					$("#step2btn").click(function() {
-						$("#expenseAmount1").slideDown(1000);
-					}); 
-
-					$("#step3btn").click(function() {
-						$("#expenseFrequency1").slideDown(1000);
-					});
-
-					$("#submitExpensebtn").click(function() {
-
-						$("#title1").hide();
-						$("#transaction1").hide();
-						$('#expenseName1').hide();
-						$('#expenseAmount1').hide();
-						$('#expenseFrequency1').hide();
-						$("#newBudget").hide();
-
-						$("#addIncome").slideDown(1000);
-						$("#newExpense").slideDown(1000);
+				});
+			});
 						
-					}); 
+			function title1() {
+				$("#title1").slideDown(1000);
+				};
 
+			function expenseName1() {
+				$("#expenseName1").slideDown(1000);
+				};
 
+			function newExpense() {				
+				$("#newExpenseName").slideDown(1000);
+				$("#newExpenseAmount").slideDown(1000);
+				$("#newExpenseFrequency").slideDown(1000);
+				};
+			function submitNewExpense(){
 
-					$("#step4btn").click(function() {
-						$("#expenseFrequency1").slideDown(1000);
-					});
+			}
 
+			$('#newBudget').click(function() {
+					title1();
+				}); 
 
+			$("#step1btn").click(function() {
+				var dataString = 'title=' + $('#simulation_title').val();
+				console.log(dataString);
+				$.post("http://finance101.dev/simulations", dataString, function(data) {
+					simulation_id = data.sim_id;
+					console.log(simulation_id);
+				});
+				expenseName1();
+			}); 
 
-					// $("#step3btn").click(function() {
-					// 	$("#expenseFrequency1").slideDown(1000);
-					// }); 
+			$("#step2btn").click(function() {
+				$("#expenseAmount1").slideDown(1000);
+			}); 
 
-				 }); 
+			$("#step3btn").click(function() {
+				$('#expense_type').slideDown(1000);
+			});
 
-				</script>
+			$('#step4btn').click(function () {
+				$("#expenseFrequency1").slideDown(1000);
+			});
 
+			$("#submitExpensebtn").click(function(e) {
+				e.preventDefault();
+				//make datastring with columns for transactions table
+				var dataString = 'title=' + $('#transaction_title').val() + '&amount=' + $('#transaction_amount').val() + '&type=' + $('#transaction_type').val() + '&frequency=' + $('#transaction_frequency').val() + '&simulation_id=' + simulation_id;
+				//include simulation_id
+				console.log(dataString);
+				$.post('http://finance101.dev/transactions', dataString, function(data) {
+					console.log(data.message);
+					console.log(data.approx_daily)
+				});
+				
+				$('.input_field').val('');
+				$('.form-piece').hide();
+				$('.transaction_form').slideDown(1000);
+			}); 
 
-
-
+			// $("#step3btn").click(function() {
+			// 	$("#expenseFrequency1").slideDown(1000);
+			// }); 
+		 }); 
+	</script>
 @stop
 
 

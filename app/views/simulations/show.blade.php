362cd7e30581e6d0c75244f42934101dbcf24afe
Frank Rodriguez <frank.aaron.rodriguez@gmail.com>
2014-11-02 20:00:48 -0600

    Finished add and delete features and charts update correctly. Edit still doesn't work.

--- app/views/simulations/show.blade.php
+++ app/views/simulations/show.blade.php
@@ -9,7 +9,7 @@
 
 	<div id="pieChart"></div>
 	
-	<table class="table table-striped">
+	<table id="transactions-table" class="table table-striped">
 		<tr>
 			<th>Title</th>
 			<th>Amount</th>
@@ -21,13 +21,13 @@
 		
 		@foreach($simulation->transaction as $transaction)
 			<tr data-transactionId='{{ $transaction->id }}'>
-				<td>{{{ $transaction->title }}}</td>
+				<td class="transaction-title">{{{ $transaction->title }}}</td>
 				
-				<td>{{{ $transaction->amount }}}</td>
+				<td class="transaction-amount">{{{ $transaction->amount }}}</td>
 				
-				<td>{{{ $transaction->type }}}</td>
+				<td class="transaction-type">{{{ $transaction->type }}}</td>
 				
-				<td>{{{ $transaction->frequency }}}</td>
+				<td class="transaction-frequency">{{{ $transaction->frequency }}}</td>
 				
 				<td><button class="edit-button" data-transactionId='{{ $transaction->id }}'>Edit</button></td>
 				
@@ -46,7 +46,7 @@
 	<script type="text/javascript">
 		$(document).ready(function () {
 			var startDate = moment('{{{ $simulation->user->created_at }}}'),
-				endDate,
+				endDate = moment(),
 				distance,
 				chart,
 				simulations = [{
@@ -55,70 +55,138 @@
 				}],
 				domain = '{{{ $_ENV['DOMAIN'] }}}',
 				transactions = [],
-				pieData = [],
-				debits = [],
-				income = 0,
 				simulation_id = {{{ $simulation->id }}},
-				leftovers = 100;
+				editId;
+				
 				@foreach($simulation->transaction as $transaction)
 					transactions.push({
+							'id' : '{{{ $transaction->id }}}',
 							'title' : '{{{ $transaction->title }}}',
 							'amount' : '{{{ $transaction->amount }}}',
 							'type' : '{{{ $transaction->type }}}',
-							'frequency' : '{{{ $transaction->frequency }}}'
+							'frequency' : '{{{ $transaction->frequency }}}',
 					});
 				@endforeach
 
 			// New transaction button
 			$('#new-transaction-submit').click(function () {
-				var dataString = 'title=' + $('#transaction-title').val() + '&amount=' + $('#transaction-amount').val() + '&type=' + $('#transaction-type').val() + '&frequency=' + $('#transaction-frequency').val() + '&simulation_id=' + simulation_id;
+				var newTitle = $('#transaction-new-title').val(),
+					newAmount = $('#transaction-new-amount').val(),
+					newType = $('#transaction-new-type').val(),
+					newFrequency = $('#transacton-new-frequency').val(),
+					newId,
+					dataString = 'title=' + newTitle + '&amount=' + newAmount + '&type=' + newType + '&frequency=' + newFrequency + '&simulation_id=' + simulation_id;
+				console.log(dataString);
 				$.post(domain + 'transactions', dataString, function (data) {
 					if (data.success) {
-						console.log(data.message);						
+						console.log(data.message);
+						console.log(data.newId);
+						newId = data.newId;
+						console.log(data.approx_daily);
+						simulations[0].approx_daily_value = data.approx_daily;
+						displayLineGraph();
+						$('#transactions-table').append(
+							'<tr data-transactionId=' + newId + '><td class="transaction-title">' + newTitle + '</td><td class="transaction-amount">' + newAmount + '</td><td class="transaction-type">' + newType + '</td><td class="transaction-frequency">' + newFrequency + '</td><td><button class="edit-button" data-transactionId=' + newId + '>Edit</button></td><td><button class="delete-button" data-transactionId=' + newId + '>Delete</button></td></tr>');
+						$('.delete-button').click(attemptDelete);
 					}
 				});
+				$('#transactions-create').modal('hide');
+				// update table
+
+				
+				// update line graph
+
+
+				// update pie graph
+				transactions.push({
+					'id' : newId,
+					'title' : newTitle,
+					'amount' : newAmount,
+					'type' : newType,
+					'frequency' : newFrequency
+				});
+				displayPieChart();
 			});
 
 			// Calculates total monthly income and creates array of debits so that debits can be represented as a share of income in the pie chart
-			transactions.forEach(function (transaction, index, array) {
-				var amount = 0;
-				console.log(transaction.frequency);
-				switch(transaction.frequency) {
-					case 'daily' : 
-						amount = transaction.amount * 30;
-						break;
-					case 'weekly' :
-						amount = transaction.amount * 4;
-						break;
-					case 'monthly' :
-						amount = transaction.amount;
-						break;
-				}
-				console.log(amount);
+			function displayPieChart () {
+				var debits = [],
+					income = 0,
+					leftovers = 100,
+					pieData = [];
+				transactions.forEach(function (transaction, index, array) {
+					console.log(transaction.frequency);
 
-				if (transaction.type == 'credit') {
-					income += amount;
-				} else {
-					debits.push(transaction);
-				}
-			});
+					switch(transaction.frequency) {
+						case 'daily' : 
+							amount = transaction.amount * 30;
+							break;
+						case 'weekly' :
+							amount = transaction.amount * 4;
+							break;
+						case 'monthly' :
+							amount = transaction.amount;
+							break;
+					}
+					console.log(amount);
 
-			debits.forEach(function (debit, index, array) {
-				var share = Math.round(debit.amount * 100 / income)
-				var newData = [debit.title, share];
-				leftovers -= share;
-				pieData.push(
-					newData
-				);
-			});
+					if (transaction.type == 'credit') {
+						income += amount;
+					} else {
+						debits.push(transaction);
+					}
+				});
 
-			pieData.push(['Surplus', leftovers]);
+				debits.forEach(function (debit, index, array) {
+					var share = Math.round(debit.amount * 100 / income)
+					var newData = [debit.title, share];
+					leftovers -= share;
+					pieData.push(
+						newData
+					);
+				});
 
-			// Delete button
-			$('.delete-button').click(function () {
+				pieData.push(['Surplus', leftovers]);
+
+				// Pie chart
+				$('#pieChart').highcharts({
+						chart: {
+							plotBackgroundColor: null,
+							plotBorderWidth: 1,//null,
+							plotShadow: false
+						},
+						title: {
+							text: 'How this budget is split'
+						},
+						tooltip: {
+							pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+						},
+						plotOptions: {
+							pie: {
+								allowPointSelect: true,
+								cursor: 'pointer',
+								dataLabels: {
+									enabled: true,
+									format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+									style: {
+										color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+									}
+								}
+							}
+						},
+						series: [{
+							type: 'pie',
+							name: 'Income',
+							data: pieData
+						}]
+					});
+			}	
+
+			function attemptDelete() {
 				var id;
 				if(confirm('Delete This Transaction?')) {
 					id = $(this).attr('data-transactionId');
+					console.log(id);
 					$.ajax({
 						url : domain + 'transactions/' + id,
 						type : 'post',
@@ -127,50 +195,70 @@
 						},
 						success : function (data) {
 							console.log(data.message);
+							console.log(data.approx_daily);
+							simulations[0].approx_daily_value = data.approx_daily;
 						}
 					});
 					$('tr[data-transactionId=' + id + ']').remove();
+					// update line graph
+					transactions.forEach(function (transaction, index, array) {
+						if (transaction.id == id) {
+							array.splice(index, 1);
+							console.log(transactions);
+						}
+					});
+					displayLineGraph();
+
+					// update pie graph
+					displayPieChart();
 				}
-			});
+			}
+
+			// Delete button
+			$('.delete-button').click(attemptDelete);
 
 			// Edit button
 			$('.edit-button').click(function () {
-				var id = $(this).attr('data-transactionId');
+				editId = $(this).attr('data-transactionId');
+				// Set modal inputs equal to current values
+				var tableEntry = $('tr[data-transactionId=' + editId + ']');
+				$('#transaction-edit-amount').val(tableEntry.children('.transaction-amount').text());
+				$('#transaction-edit-title').val(tableEntry.children('.transaction-title').text());
+				$('#transacton-edit-frequency').val(tableEntry.children('.transaction-frequency').text());
+				$('#transaction-edit-type').val(tableEntry.children('.transaction-type').text());
+				// Display edit modal
 				$('#transactions-edit').modal();
 			});
 
-			// Pie chart
-			$('#pieChart').highcharts({
-					chart: {
-						plotBackgroundColor: null,
-						plotBorderWidth: 1,//null,
-						plotShadow: false
-					},
-					title: {
-						text: 'How this budget is split'
-					},
-					tooltip: {
-						pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-					},
-					plotOptions: {
-						pie: {
-							allowPointSelect: true,
-							cursor: 'pointer',
-							dataLabels: {
-								enabled: true,
-								format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-								style: {
-									color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-								}
+			//Edit modal submit
+			$('#transaction-edit-submit').click(function () {
+				var dataString = {
+					_method : 'put'
+				};
+
+				/*'title=' + $('#transaction-edit-title').val() + '&amount=' + $('#transaction-edit-amount').val() + '&frequency=' + $('#transaction-edit-frequency').val() + '&type=' + $('#transaction-edit-type').val();*/
+				$.ajax({
+						url : domain + 'transactions/' + editId,
+						type : 'post',
+						data : {
+							'_method' : 'put',
+							'title' :  $('#transaction-edit-title').val(),
+							'amount' : $('#transaction-edit-amount').val(),
+							'type' : $('#transaction-edit-type').val(),
+							'frequency' : $('#transaction-edit-frequency').val(),
+							'simulation_id' : simulation_id
+						},
+						success : function (data) {
+							if (data.success) {
+								console.log(data.message);
 							}
 						}
-					},
-					series: [{
-						type: 'pie',
-						name: 'Income',
-						data: pieData
-					}]
-				});
+					});
+				$('#transactions-edit').modal('hide');
+				// update table
+				// update line graph
+				// update pie graph
+			});
 
 			// Projection date picker
 			$( "#toDatePicker" ).datetimepicker();
@@ -202,15 +290,12 @@
 					series: ['Pick a Date']
 				});
 			
-			// Date picker event handler updating line chart
-			$('#toDatePicker').on('dp.change', function (e) {
+			function displayLineGraph () {
 				const CHART_INTERVALS = 10;
 				var chartCategories = [],
 					chartSeries = [],
 					dayCount = [],
-					endDate,
 					distance;				
-				endDate = moment($(this).data("DateTimePicker").getDate());
 				distance = endDate.diff(startDate, 'days');
 				for(var i = 1; i <= CHART_INTERVALS; i++) {
 					dayCount.push(Math.round(distance * i / CHART_INTERVALS));
@@ -255,7 +340,16 @@
 					},
 					series: chartSeries
 				});
+			}
+
+			// Date picker event handler updating line chart
+			$('#toDatePicker').on('dp.change', function () {
+				endDate = moment($(this).data("DateTimePicker").getDate());
+				displayLineGraph();
 			});
+
+			displayLineGraph();
+			displayPieChart();
 		});	
 	</script>
 @stop
@@ -270,18 +364,18 @@
 	        <h4 class="modal-title" id="myModalLabel">New Transaction</h4>
 	      </div>
 	      <div class="modal-body">
-				<input type="text" name="transaction-title" id="transaction-title" placeholder="Name of New Transaction">			   	
+				<input type="text" name="transaction-new-title" id="transaction-new-title" placeholder="Name of New Transaction">			   	
 			    
-				<select name="transaction-frequency" id="transacton-frequency">
+				<select id="transacton-new-frequency" name="transaction-new-frequency">
 					<option value="">Frequency</option>
 					<option value="daily">Daily</option>
 					<option value="weekly">Weekly</option>
 					<option value="monthly">Monthly</option>
 				</select>
 			    
-				<input type="number" name="transaction-amount" id="transaction-amount" step="any" min="0" placeholder="Enter Amount">
+				<input type="number" name="transaction-new-amount" id="transaction-new-amount" step="any" min="0" placeholder="Enter Amount">
 
-				<select id="transaction-type">
+				<select id="transaction-new-type" name="transaction-new-type">
 					<option value="">Credit/Debit</option>
 					<option value="credit">Credit</option>
 					<option value="debit">Debit</option>
@@ -304,23 +398,23 @@
 	        <h4 class="modal-title" id="myModalLabel">New Transaction</h4>
 	      </div>
 	      <div class="modal-body">
-				<input type="text" name="transaction-title" id="transaction-title" placeholder="Name of New Transaction">			   	
+				<input type="text" name="transaction-edit-title" id="transaction-edit-title" placeholder="Name of New Transaction">			   	
 			    
-				<select name="transaction-frequency" id="transacton-frequency">
+				<select name="transaction-edit-frequency" id="transacton-edit-frequency">
 					<option value="">Frequency</option>
 					<option value="daily">Daily</option>
 					<option value="weekly">Weekly</option>
 					<option value="monthly">Monthly</option>
 				</select>
 			    
-				<input type="number" name="transaction-amount" id="transaction-amount" step="any" min="0" placeholder="Enter Amount">
+				<input type="number" name="transaction-edit-amount" id="transaction-edit-amount" step="any" min="0" placeholder="Enter Amount">
 
-				<select id="transaction-type">
+				<select id="transaction-edit-type" name="transaction-edit-amount">
 					<option value="">Credit/Debit</option>
 					<option value="credit">Credit</option>
 					<option value="debit">Debit</option>
 				</select>			    
-			    	      </div>
+	      </div>
 	      <div class="modal-footer">
 	        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
 	        <button type="button" id="transaction-edit-submit" class="btn btn-primary">Save changes</button>
